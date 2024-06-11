813380304491dad42656d014cb51b316f52d8ec4
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-06-11 22:55:07 +0600

    course card add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { ArrowDown, Star } from "lucide-react";
+import { AlignJustify, ArrowDown, Star, StarIcon } from "lucide-react";
 import Image from "next/image";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -12,6 +12,7 @@ import 'swiper/css/effect-cards';
 // import required modules
 import { EffectCards } from 'swiper/modules';
 import Profile from "@/components/Profile";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -480,7 +481,7 @@ export default function Home() {
 
           <div className="grid max-w-screen-lg lg:grid-cols-2 sm:mx-auto">
             <div className="grid grid-cols-2 gap-2 FounderImages">
-              <Profile/>
+              <Profile />
             </div>
             <div className="flex flex-col justify-center text-gray-900 border-l-2 border-indigo-500 ps-[50px] instructorDetailsBorder">
               <div>
@@ -505,6 +506,126 @@ export default function Home() {
                 </div>
               </div>
             </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className='py-24'>
+          <div className='mb-12 px-6 lg:px-8'>
+            <div className='mx-auto max-w-2xl sm:text-center'>
+              <h2 className='order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900'>
+                ExpandYourCareer{' '}
+                <span className='relative px-2 bg-green-600 text-white'>
+                  Opportunity
+                </span>{' '}
+                WithOurCourses
+              </h2>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] mb-[80px] border-0">
+            <Link href={'/'}>
+              <div className="w-full min-h-[35vh] bg-[#fff] backdrop-blur border dark:shadow-[bg-slate-700] rounded-lg p-3 shadow-sm dark:shadow-inner mb-[80px] border-green-200">
+                <Image
+                  src={'/course.jpg'}
+                  className="rounded w-full"
+                  alt="course"
+                  width={500}
+                  height={300}
+                />
+                <br />
+                <h1 className="font-Poppins text-[16px] text-gray-900">Journey Of Frontend Web Development</h1>
+                <div className="w-full flex items-center justify-between pt-2">
+                  <div className="flex mt-1 ml-2 md:mt-0 800px:ml-0">
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                  </div>
+                  <h5 className="text-gray-900 undefined"> 500  Students</h5>
+                </div>
+                <div className="w-full flex items-center justify-between pt-3">
+                  <div className="flex">
+                    <h3 className="text-gray-900">5000৳</h3>
+                    <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-gray-900">6000৳</h5>
+                  </div>
+                  <div className="flex items-center pb-3">
+                    <AlignJustify className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <h5 className="pl-2 text-gray-900">144 Lectures</h5>
+                  </div>
+                </div>
+
+              </div>
+            </Link>
+            <Link href={'/'}>
+              <div className="w-full min-h-[35vh] bg-[#fff] backdrop-blur border dark:shadow-[bg-slate-700] rounded-lg p-3 shadow-sm dark:shadow-inner mb-[80px] border-green-200">
+                <Image
+                  src={'/course.jpg'}
+                  className="rounded w-full"
+                  alt="course"
+                  width={500}
+                  height={300}
+                />
+                <br />
+                <h1 className="font-Poppins text-[16px] text-gray-900">Journey Of Frontend Web Development</h1>
+                <div className="w-full flex items-center justify-between pt-2">
+                  <div className="flex mt-1 ml-2 md:mt-0 800px:ml-0">
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                  </div>
+                  <h5 className="text-gray-900 undefined"> 500  Students</h5>
+                </div>
+                <div className="w-full flex items-center justify-between pt-3">
+                  <div className="flex">
+                    <h3 className="text-gray-900">5000৳</h3>
+                    <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-gray-900">6000৳</h5>
+                  </div>
+                  <div className="flex items-center pb-3">
+                    <AlignJustify className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <h5 className="pl-2 text-gray-900">144 Lectures</h5>
+                  </div>
+                </div>
+
+              </div>
+            </Link>
+            <Link href={'/'}>
+              <div className="w-full min-h-[35vh] bg-[#fff] backdrop-blur border dark:shadow-[bg-slate-700] rounded-lg p-3 shadow-sm dark:shadow-inner mb-[80px] border-green-200">
+                <Image
+                  src={'/course.jpg'}
+                  className="rounded w-full"
+                  alt="course"
+                  width={500}
+                  height={300}
+                />
+                <br />
+                <h1 className="font-Poppins text-[16px] text-gray-900">Journey Of Frontend Web Development</h1>
+                <div className="w-full flex items-center justify-between pt-2">
+                  <div className="flex mt-1 ml-2 md:mt-0 800px:ml-0">
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <Star className='h-4 w-4 stroke-[3px] text-green-600' />
+                  </div>
+                  <h5 className="text-gray-900 undefined"> 500  Students</h5>
+                </div>
+                <div className="w-full flex items-center justify-between pt-3">
+                  <div className="flex">
+                    <h3 className="text-gray-900">5000৳</h3>
+                    <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-gray-900">6000৳</h5>
+                  </div>
+                  <div className="flex items-center pb-3">
+                    <AlignJustify className='h-4 w-4 stroke-[3px] text-green-600' />
+                    <h5 className="pl-2 text-gray-900">144 Lectures</h5>
+                  </div>
+                </div>
+
+              </div>
+            </Link>
           </div>
         </MaxWidthWrapper>
       </section>
