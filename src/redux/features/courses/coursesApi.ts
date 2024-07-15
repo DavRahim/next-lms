7c7ccd3ada90d72d7b708192f4aaf9865f4a6c5b
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-07-15 20:33:41 +0600

    api connect redux

--- src/redux/features/courses/coursesApi.ts
+++ src/redux/features/courses/coursesApi.ts
@@ -23,8 +23,40 @@ export const courseApi = apiSlice.injectEndpoints({
                 credentials: "include" as const
             })
         }),
+        addNewQuestion: builder.mutation({
+            query: ({ question, courseId, contentId }) => ({
+                url: `/course/add-question`,
+                method: "PUT",
+                body: { question, courseId, contentId },
+                credentials: "include" as const
+            })
+        }),
+        addAnswerInQuestion: builder.mutation({
+            query: ({ answer, courseId, contentId, questionId }) => ({
+                url: `/course/add-answer`,
+                method: "PUT",
+                body: { answer, courseId, contentId, questionId },
+                credentials: "include" as const
+            })
+        }),
+        addReviewInCourse: builder.mutation({
+            query: ({ review, rating, courseId }) => ({
+                url: `/course/add-review/${courseId}`,
+                method: "PUT",
+                body: { review, rating },
+                credentials: "include" as const
+            })
+        }),
+        addReplyInReview: builder.mutation({
+            query: ({ reviewId, comment, courseId }) => ({
+                url: `/course/add-reply`,
+                method: "PUT",
+                body: { reviewId, comment, courseId },
+                credentials: "include" as const
+            })
+        }),
     })
 })
 
 
-export const { useGetUsersAllCoursesQuery, useGetCourseContentQuery, useGetCoursesDetailsQuery } = courseApi
+export const { useGetUsersAllCoursesQuery, useGetCourseContentQuery, useGetCoursesDetailsQuery, useAddNewQuestionMutation, useAddAnswerInQuestionMutation, useAddReviewInCourseMutation, useAddReplyInReviewMutation } = courseApi;
