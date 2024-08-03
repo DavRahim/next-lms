4d51d9fc31b2273493c7cc1e93bc04512ddbbf80
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-08-03 21:55:04 +0600

    auth fix

--- src/app/(user-account)/enrolment/page.tsx
+++ src/app/(user-account)/enrolment/page.tsx
@@ -1,8 +1,8 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import { Separator } from "@/components/ui/separator";
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { redirect } from "next/navigation";
-import React from "react";
-import { useSelector } from "react-redux";
 
 
 
